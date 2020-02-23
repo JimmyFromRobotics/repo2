1d72fe998d2ccd6619c6ae0aaa7cc15b99cf66ff
JimmyFromRobotics <34200973+JimmyFromRobotics@users.noreply.github.com>
2020-02-22 21:31:05 -0500

    Delete index.js

--- bootcamp4-react-JimmyFromRobotics-master/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App3 from './App3';
-import App2 from './App2';
-import data from './data/data';
-
-//The app is now passing the data as a property
-ReactDOM.render(<App3 data={data} />, document.getElementById('root'));
